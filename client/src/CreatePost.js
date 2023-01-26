4ab31afdd1786a9d05e7aac407b36b7f56d1ddf3
Bailey <baileyrosefern@gmail.com>
2023-01-26 10:11:26 -0700

    Got the shelf image imported!

--- client/src/CreatePost.js
+++ client/src/CreatePost.js
@@ -1,13 +1,18 @@
 import {Button} from "react-bootstrap";
-import {useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom';
+import Images from './image'
+
 
 function CreatePost() {
     const navigate = useNavigate();
+    
 
     return (
         <div style={{width:"90%", margin: "auto auto",textAlign:"center" }}>
             {/* IF YOUR STYLING IS WEIRD LOOK HERE -Not best practice*/}
-            <h1>Create a material</h1>
+            <h1>Your shelf</h1>
+               <Images />
+
             <Button
             style={{width:"100%"}} 
             variant="outline-dark"
---
+++ client/src/image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import shelfImage from './images/side_table.png'
+
+function Images() {
+    return (
+        <div>
+            <img src ={shelfImage} alt="" />
+        </div>
+    )
+}
+
+export default Images;
